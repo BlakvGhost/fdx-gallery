e0071771d5c0cc679e66d8551798c3dbcabfa0e0
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-04-10 17:42:38 +0100

    Add Route To Auth endpoints

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,34 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/auth/LoginView.vue'
+import RegisterView from '@/views/auth/RegisterView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'login',
-      component: LoginView
+      name: 'home',
+      redirect: {name: 'auth'}
+    },
+    {
+      path: '/auth',
+      children: [
+        {
+          path: '',
+          name:'auth',
+          redirect: {name: 'login'}
+        },
+        {
+          path: 'register',
+          name:'register',
+          component: RegisterView
+        },
+        {
+          path:'login',
+          name:'login',
+          component: LoginView
+        }
+      ]
     },
   ]
 })
---
+++ src/views/auth/RegisterView.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container-fluid bg-dark p-0" style="height:100vh">
+        <div class="row justify-content-center h-100 w-100 p-0 m-0">
+            <div class="col-md-6 col-sm-10 col-xs-12 m-auto fv-sm-w-100">
+                <div class="card bg-secondary shadow">
+                    <div class="card-header">
+                        <div class="p-2 w-50 mx-auto">
+                            <div class="fw-bold m-0 border-0 text-center">
+                                <a href="javascript:void(0)"
+                                    class="btn-load text-decoration-none text-white col-8 col">GalleryApp</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body">
+                        <h5 class="text-center card-title text-white opacity-50">Ouverture de votre compte</h5>
+                        <div class="my-2">
+                            <div class="my-3 text-center">
+                                <h4 class="text-danger animated shake">
+                                    <!-- <?= AuthServiceProvider::printSessionMessage() ?> -->
+                                </h4>
+                            </div>
+                            <form action="/user/app/register" method="post">
+                                <div class="my-2">
+                                    <input type="email" required class="form-control bg-light" id="userEmail" name="email"
+                                        placeholder="Your Email *">
+                                </div>
+                                <div class="my-2">
+                                    <input type="text" required class="form-control bg-light" id="username" name="username"
+                                        placeholder="Your Username *">
+                                </div>
+                                <div class="my-2 input-group">
+                                    <input type="password" required class="form-control bg-light" name="password1"
+                                        placeholder="Your Password">
+                                    <button type="button" class="btn btn-danger password-visible"><i
+                                            class="mdi mdi-eye-outline"></i></button>
+                                </div>
+                                <div class="my-2 input-group">
+                                    <input type="password" required class="form-control bg-light" name="password2"
+                                        placeholder="Confirm Password">
+                                    <button type="button" class="btn btn-danger password-visible"><i
+                                            class="mdi mdi-eye-outline"></i></button>
+                                </div>
+                                <div class="my-2 d-flex justify-content-between">
+                                    <router-link :to="{name: 'login'}" class="btn btn-link text-white">Already have account ?</router-link>
+                                </div>
+                                <div class="my-3 text-center">
+                                    <button class="btn btn-dark w-50" name="submit" type="submit">Create Your
+                                        Account</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
