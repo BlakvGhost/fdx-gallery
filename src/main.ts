0fe147ed44df69d378ef41188e2ba6aeedb727ea
Kabirou ALASSANE <kabirou2001@gmail.com>
2023-04-11 14:48:35 +0100

    #Conf Firebase

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,9 @@ import 'bootstrap/dist/js/bootstrap.js'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
-
 const app = createApp(App)
 
 app.use(createPinia())
-app.use(router)
+app.use(router);
 
 app.mount('#app')
